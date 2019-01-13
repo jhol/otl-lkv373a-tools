07f908140fa1ed9e0b96b96bd5aea4352fad1ecc
Joel Holdsworth <joel@airwebreathe.org.uk>
2019-01-13 20:25:30 +0000

    Added monitor-tty

--- lkv373a/__init__.py
+++ lkv373a/__init__.py
@@ -1,6 +1,47 @@
 
 from . import vserprog
 
+def _find_tty_device():
+  import os
+  from os import readlink, path
+  from os.path import dirname as dn
+
+  tty_class_dir = '/sys/class/tty'
+
+  def tty_usb_interface_dir(tty):
+    tty_device_dir = path.join(tty_class_dir, readlink(path.join(tty_class_dir, tty)))
+    device = path.abspath(path.join(tty_device_dir, readlink(path.join(tty_device_dir, 'device'))))
+    if path.basename(device).startswith('tty'):
+      return dn(device)
+    return device
+
+  def tty_usb_bus(tty):
+    try:
+      with open(path.join(dn(tty_usb_interface_dir(tty)), 'busnum'), 'r') as f:
+        return f.read()
+    except FileNotFoundError:
+      return None
+
+  def usb_device_id(device_dir):
+    with open(path.join(device_dir, 'idVendor'), 'r') as vid_f:
+      with open(path.join(device_dir, 'idProduct'), 'r') as pid_f:
+        return (vid_f.read().strip(), pid_f.read().strip())
+
+  try:
+    vserprog_device = next(iter(vserprog.devices.values()))
+  except StopIteration:
+    return None
+
+  tty_devices = os.listdir(tty_class_dir)
+  tty_usb_busses = {tty: tty_usb_bus(tty) for tty in tty_devices}
+  rig_bus_num = tty_usb_busses[vserprog_device]
+  sibling_ttys = {tty for tty in tty_devices if tty_usb_busses[tty] == rig_bus_num}
+  sibling_tty_ids = [(tty, usb_device_id(dn(tty_usb_interface_dir(tty)))) for tty in sibling_ttys]
+
+  return [tty for tty in sibling_tty_ids if tty[1] == ('067b', '2303')][0][0]
+
+tty_device = _find_tty_device()
+
 def reset():
   with open('/dev/{}'.format(vserprog.devices['u2']), 'wb') as f:
     f.write(b'\x16')
--- lkv373a/vserprog.py
+++ lkv373a/vserprog.py
@@ -4,13 +4,13 @@ import tempfile
 
 empty_file = tempfile.NamedTemporaryFile()
 
-tty_class_dir = '/sys/class/tty'
 vserprog_ids = {'234218677285525154FF6E06': 'mu1', '164023677572545554FF6B06': 'u2'}
 
 def _list_vserprogs():
   from os import path, readlink
   from os.path import dirname as dn
 
+  tty_class_dir = '/sys/class/tty'
   tty_devices = os.listdir(tty_class_dir)
   vserprog_devices = [
     (d, open(path.join(tty_class_dir, dn(dn(dn(readlink(path.join(tty_class_dir, d))))), 'serial'), 'r').read().strip())
