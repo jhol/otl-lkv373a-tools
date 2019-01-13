ae693c8e73669fe1ed5aacbf507a8310932343e7
Joel Holdsworth <joel@airwebreathe.org.uk>
2019-01-13 21:23:27 +0000

    Add redacted output

--- test-startup.py
+++ test-startup.py
@@ -6,6 +6,7 @@ from lkv373a import vserprog
 import serial
 import sys
 import time
+import re
 
 timeout=3
 lines=32
@@ -20,8 +21,9 @@ vserprog.trisate_vserprogs(vserprog_devices)
 print('Begin...')
 start_time = time.time()
 with open(sys.argv[1], 'w') as log:
+  log.write('#,Lines,Hash,Redacted Hash\n')
   for i in range(128):
-    sys.stdout.write('{: 5.1f} #{: 3}. '.format(time.time() - start_time, i))
+    sys.stdout.write('[{: 5.1f}] {: 3}. '.format(time.time() - start_time, i))
     sys.stdout.flush()
     lkv373a.reset()
 
@@ -32,5 +34,11 @@ with open(sys.argv[1], 'w') as log:
       with open('{}.asc'.format(capture_hash), 'wb') as f:
         f.write(capture)
 
-      sys.stdout.write('{}-lines, {}\n'.format(len(capture_lines), capture_hash))
-      log.write('{},{},{}'.format(i, lines, capture_hash))
+      capture_redacted = re.sub('[x ][a-f0-9]+', ' ----', capture.decode('ascii')).encode('ascii')
+      capture_redacted_hash = hashlib.md5(capture_redacted).hexdigest()
+      with open('{}.asc'.format(capture_redacted_hash), 'wb') as f:
+        f.write(capture_redacted)
+
+      sys.stdout.write('{}-lines, {}, redacted={}\n'.format(len(capture_lines), capture_hash, capture_redacted_hash))
+      log.write('{},{},{},{}\n'.format(i, lines, capture_hash, capture_redacted_hash))
+      log.flush()
