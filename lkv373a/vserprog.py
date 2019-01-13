3b04eb69425e23942894e867156fabee52824f23
Joel Holdsworth <joel@airwebreathe.org.uk>
2019-01-13 15:28:20 +0000

    Added tristate-vserprogs

--- lkv373a/vserprog.py
+++ lkv373a/vserprog.py
@@ -1,5 +1,8 @@
 import os
+import subprocess
+import tempfile
 
+empty_file = tempfile.NamedTemporaryFile()
 
 tty_class_dir = '/sys/class/tty'
 vserprog_ids = {'234218677285525154FF6E06': 'mu1', '164023677572545554FF6B06': 'u2'}
@@ -14,3 +17,11 @@ def list_vserprogs():
     for d in tty_devices if d.startswith('ttyACM')]
 
   return {vserprog_ids[kv[1]]: kv[0] for kv in vserprog_devices}
+
+def spawn_tristate_vserprog(device):
+  return subprocess.Popen(['flashrom', '-p', 'serprog:dev=/dev/{}:40000000'.format(device), '-v', empty_file.name],
+      stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+def trisate_vserprogs(devices):
+  for p in [spawn_tristate_vserprog(device) for device in devices]:
+    p.wait()
