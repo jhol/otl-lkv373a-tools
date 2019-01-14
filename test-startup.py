3cda1b500bac17cef6b6ca2a683ea1b3f6518e80
Joel Holdsworth <joel@airwebreathe.org.uk>
2019-01-14 10:49:30 +0000

    Initial test run

--- test-startup.py
+++ test-startup.py
@@ -4,12 +4,15 @@ import hashlib
 import lkv373a
 from lkv373a import vserprog
 import serial
+import subprocess
 import sys
 import time
+import tempfile
 import re
 
 timeout=3
 lines=32
+u2_firmware_path='/home/joel/opentechlab/16-it9919/20181226-lkv-373a-backups/u2.bin'
 
 vserprog_devices = vserprog.devices.values()
 if len(vserprog_devices) != 2:
@@ -22,9 +25,25 @@ print('Begin...')
 start_time = time.time()
 with open(sys.argv[1], 'w') as log:
   log.write('#,Lines,Hash,Redacted Hash\n')
-  for i in range(128):
+  for i in range(4096):
     sys.stdout.write('[{: 5.1f}] {: 3}. '.format(time.time() - start_time, i))
     sys.stdout.flush()
+
+    with open('mod-firmware.bin', 'wb') as f:
+      mod_firmware = bytearray(open(u2_firmware_path, 'rb').read())
+      mod_firmware[i] = 0xFF ^ mod_firmware[i]
+      f.write(mod_firmware)
+
+    while True:
+      flashrom = subprocess.Popen(['flashrom', '-p', 'serprog:dev=/dev/{}:40000000'.format(vserprog.devices['u2']),
+          '-w', 'mod-firmware.bin'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+      stdout, _ = flashrom.communicate()
+      if flashrom.returncode != 0:
+        sys.stderr.write('flashrom failed!\n')
+        sys.stderr.write(stdout.decode('utf-8'))
+      else:
+        break
+
     lkv373a.reset()
 
     with serial.Serial('/dev/{}'.format(lkv373a.tty_device), 115200, timeout=timeout) as ser:
@@ -34,10 +53,13 @@ with open(sys.argv[1], 'w') as log:
       with open('{}.asc'.format(capture_hash), 'wb') as f:
         f.write(capture)
 
-      capture_redacted = re.sub('[x ][a-f0-9]+', ' ----', capture.decode('ascii')).encode('ascii')
-      capture_redacted_hash = hashlib.md5(capture_redacted).hexdigest()
-      with open('{}.asc'.format(capture_redacted_hash), 'wb') as f:
-        f.write(capture_redacted)
+      try:
+        capture_redacted = re.sub('[x ][a-f0-9]+', ' ----', capture.decode('ascii')).encode('ascii')
+        capture_redacted_hash = hashlib.md5(capture_redacted).hexdigest()
+        with open('{}.asc'.format(capture_redacted_hash), 'wb') as f:
+          f.write(capture_redacted)
+      except UnicodeDecodeError:
+        capture_redacted_hash = '-'
 
       sys.stdout.write('{}-lines, {}, redacted={}\n'.format(len(capture_lines), capture_hash, capture_redacted_hash))
       log.write('{},{},{},{}\n'.format(i, lines, capture_hash, capture_redacted_hash))
