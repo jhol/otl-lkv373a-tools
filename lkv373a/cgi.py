099b5580cdca8cef236185840528d2b568848808
Joel Holdsworth <joel@airwebreathe.org.uk>
2019-05-26 17:01:23 +0100

    cgi.py: Move out timeout

--- lkv373a/cgi.py
+++ lkv373a/cgi.py
@@ -1,10 +1,8 @@
 
 import aiohttp
-import async_timeout
 
 async def upgrade_encoder(host, file):
   async with aiohttp.ClientSession() as session:
-    async with async_timeout.timeout(60):
-      async with session.post('http://{}/dev/encinfo.cgi'.format(host),
-          data={'filename' : open(file, 'rb')}) as r:
-        r.raise_for_status()
+    async with session.post('http://{}/dev/encinfo.cgi'.format(host),
+        data={'filename' : open(file, 'rb')}) as r:
+      r.raise_for_status()
